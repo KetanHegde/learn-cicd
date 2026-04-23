1e7a41dcfa5ac7dced013580393b9d360d455ca0
KetanHegde <ketanhegde2003@gmail.com>
2026-04-23 16:42:52 +0530

    Change app content

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello from CI/CD\nI am Ketan")
+        self.wfile.write(b"Hello from CI/CD\nI am Ketan\nI am good")
 
 server = HTTPServer(("0.0.0.0", 8080), Handler)
 print("Server started on port 8080")
